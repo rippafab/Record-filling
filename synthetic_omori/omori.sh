d76e0d23a355a002462c97b8a8c64f125b8fe371
rippafab <fabrizio.rippa@unicampania.it>
2026-06-08 01:12:58 +0200

    Add files via upload

---
+++ synthetic_omori/omori.sh
@@ -0,0 +1,30 @@
+gfortran -O3 pp3c.f randgen.f
+./a.out
+cp cat.dat c1.dat
+cp cat2.dat c3.dat
+gfortran legge_01.f90
+./a.out
+gfortran -O3 pp3d2.f randgen.f
+./a.out
+cp cat.dat c2.dat
+gfortran -O3 trasforma_file_ingresso.f90
+./a.out
+cp c4.dat cat2.dat
+rm catq2.dat
+gfortran -O3 legge_qe_fit_exp3.f90
+./a.out
+gfortran legge_02.f90
+./a.out
+gfortran -O3 legge_refilling6_runningthreshold.f90
+./a.out
+cp catq2.dat c1.dat
+cp cat2.dat c2.dat
+cp cat_filled.dat c3.dat
+gfortran -O3 trasforma_file_ingresso.f90
+./a.out
+gfortran legge_2.f90
+./a.out
+gfortran legge_1.f90
+./a.out
+gfortran legge_1un.f90
+./a.out
